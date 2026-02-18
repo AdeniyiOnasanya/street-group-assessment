5c33999e9441450d359f1e1205928d7067c1e19f
David-Onas <david.onasanya@nextlevelsoftware.co.uk>
2026-02-18 05:08:42 +0000

    Added Project Base

---
+++ resources/js/types/Homeowner.ts
@@ -0,0 +1,6 @@
+export interface Homeowner{
+    title: string
+    first_name: string |  null
+    last_name: string
+    initial: string | null
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<! Doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Homeowner Name Formatter"</title>
+        @vite(["resources/css/app.css", "resources/js/app.ts"])
+    </head>
+    <body class="bg-slate-50 text-slate-900">
+        <div id="app"></div>
+    </body>
+</html>
